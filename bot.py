b7927666f6a546ea9988e23e123ebc2b165afc16
scollins83 <saracollins0508@gmail.com>
2020-05-31 21:53:18 -0500

    Updated discord load variables

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import os
 import random
 
 from discord.ext import commands
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='%')
